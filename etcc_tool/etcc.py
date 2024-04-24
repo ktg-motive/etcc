c5e4380ddc7d455f3f6dbd39e3cd541c1f352855
“ktg-motive” <kai@motiveapp.co>
2024-04-24 11:05:17 -0500

    update apps

--- etcc_tool/etcc.py
+++ etcc_tool/etcc.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Flask, Blueprint, render_template, request, redirect, url_for
 import sqlite3
 import pandas as pd
 from flask import jsonify
